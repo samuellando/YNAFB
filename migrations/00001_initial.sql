ef41503aa15c72ff05d83ba298ca3e167a9f204d
Samuel Lando <samuel.lando@icloud.com>
2026-09-05 11:17:50 -0400

    Update schemas
    Budget scoping, and user account logins

--- migrations/00001_initial.sql
+++ migrations/00001_initial.sql
@@ -1,111 +1,226 @@
 -- +goose up
+CREATE TABLE login (
+  id INTEGER PRIMARY KEY,
+  username TEXT NOT NULL UNIQUE CHECK (LENGTH(username) >= 3),
+  password TEXT NOT NULL
+);
+
 CREATE TABLE budget (
-    id INTEGER PRIMARY KEY,
-    name TEXT NOT NULL UNIQUE CHECK (name <> '')
+  id INTEGER PRIMARY KEY,
+  login_id INTEGER NOT NULL REFERENCES login (id) ON DELETE CASCADE,
+  name TEXT NOT NULL CHECK (LENGTH(name) >= 3),
+  UNIQUE (login_id, name)
 );
 
 CREATE TABLE account (
-    id INTEGER PRIMARY KEY,
-    budget INTEGER NOT NULL REFERENCES budget (id) ON DELETE CASCADE,
-    name TEXT NOT NULL CHECK (name <> ''),
-    UNIQUE (budget, name)
-);
-
-CREATE TABLE "transaction" (
-    id INTEGER PRIMARY KEY,
-    date UNIX_EPOCH_INTEGER NOT NULL,
-    account INTEGER NOT NULL REFERENCES account (id) ON DELETE CASCADE,
-    payee INTEGER NOT NULL REFERENCES payee (id) ON DELETE CASCADE,
-    total_outflow INTEGER NOT NULL DEFAULT 0,
-    total_inflow INTEGER NOT NULL DEFAULT 0,
-    note TEXT NOT NULL DEFAULT ''
-    CHECK (total_inflow = 0 OR total_outflow = 0),
-    CHECK (total_inflow >= 0 AND total_outflow >= 0)
+  id INTEGER PRIMARY KEY,
+  budget_id INTEGER NOT NULL REFERENCES budget (id) ON DELETE CASCADE,
+  name TEXT NOT NULL CHECK (LENGTH(name) >= 3),
+  UNIQUE (budget_id, id),
+  UNIQUE (budget_id, name)
 );
 
 CREATE TABLE payee (
-    id INTEGER PRIMARY KEY,
-    budget INTEGER NOT NULL REFERENCES budget (id) ON DELETE CASCADE,
-    name TEXT NOT NULL CHECK (name <> ''),
-    UNIQUE (budget, name)
+  id INTEGER PRIMARY KEY,
+  budget_id INTEGER NOT NULL REFERENCES budget (id) ON DELETE CASCADE,
+  name TEXT NOT NULL CHECK (LENGTH(name) >= 3),
+  UNIQUE (budget_id, id),
+  UNIQUE (budget_id, name)
 );
 
-CREATE TABLE "transaction_category" (
-    id INTEGER PRIMARY KEY,
-    "transaction" INTEGER NOT NULL REFERENCES "transaction" (id) ON DELETE CASCADE,
-    other_account INTEGER REFERENCES account (id) ON DELETE CASCADE,
-    category INTEGER REFERENCES category (id) ON DELETE CASCADE,
-    income BOOL NOT NULL DEFAULT false,
-    outflow INTEGER NOT NULL DEFAULT 0,
-    inflow INTEGER NOT NULL DEFAULT 0,
-    -- XOR, can only be one of category (spend), transfer, or income
-    CHECK (
-        (CASE WHEN other_account IS NOT NULL THEN 1 ELSE 0 END
-       + CASE WHEN category IS NOT NULL THEN 1 ELSE 0 END
-       + CASE WHEN income THEN 1 ELSE 0 END) = 1
-    ),
-    CHECK (
-        NOT INCOME OR (outflow = 0 and inflow > 0)
-    ),
-    CHECK (inflow = 0 OR outflow = 0),
-    CHECK (inflow >= 0 AND outflow >= 0)
+CREATE TABLE trx (
+  id INTEGER PRIMARY KEY,
+  budget_id INTEGER NOT NULL,
+  account_id INTEGER NOT NULL,
+  payee_id INTEGER NOT NULL,
+  date UNIX_EPOCH_INTEGER NOT NULL,
+  total_outflow INTEGER NOT NULL DEFAULT 0,
+  total_inflow INTEGER NOT NULL DEFAULT 0,
+  note TEXT NOT NULL DEFAULT '',
+  UNIQUE (budget_id, id),
+  CHECK (
+    (
+      total_inflow > 0
+      AND total_outflow = 0
+    )
+    OR (
+      total_outflow > 0
+      AND total_inflow = 0
+    )
+  ),
+  FOREIGN KEY (budget_id, account_id) REFERENCES account (budget_id, id) ON DELETE CASCADE,
+  FOREIGN KEY (budget_id, payee_id) REFERENCES payee (budget_id, id) ON DELETE CASCADE
+);
+
+CREATE TABLE trx_line (
+  id INTEGER PRIMARY KEY,
+  budget_id INTEGER NOT NULL,
+  trx_id INTEGER NOT NULL,
+  dest_account_id INTEGER,
+  category_id INTEGER,
+  income BOOL NOT NULL DEFAULT false,
+  outflow INTEGER NOT NULL DEFAULT 0,
+  inflow INTEGER NOT NULL DEFAULT 0,
+  -- XOR, can only be one of category (spend), transfer, or income
+  CHECK (
+    (
+      CASE
+        WHEN dest_account_id IS NOT NULL THEN 1
+        ELSE 0
+      END + CASE
+        WHEN category_id IS NOT NULL THEN 1
+        ELSE 0
+      END + CASE
+        WHEN income THEN 1
+        ELSE 0
+      END
+    ) = 1
+  ),
+  CHECK (
+    NOT INCOME
+    OR (
+      outflow = 0
+      and inflow > 0
+    )
+  ),
+  CHECK (
+    (
+      inflow > 0
+      AND outflow = 0
+    )
+    OR (
+      outflow > 0
+      AND inflow = 0
+    )
+  ),
+  FOREIGN KEY (budget_id, trx_id) REFERENCES trx (budget_id, id) ON DELETE CASCADE,
+  FOREIGN KEY (budget_id, dest_account_id) REFERENCES account (budget_id, id) ON DELETE CASCADE,
+  FOREIGN KEY (budget_id, category_id) REFERENCES category (budget_id, id) ON DELETE CASCADE
 );
 
 CREATE TABLE category (
-    id INTEGER PRIMARY KEY,
-    budget INTEGER NOT NULL REFERENCES budget (id) ON DELETE CASCADE,
-    name TEXT NOT NULL CHECK (name <> ''), 
-    category_group INTEGER REFERENCES category_group (id) ON DELETE SET NULL,
-    UNIQUE (budget, name),
-    CHECK (lower(name) IS NOT 'income')
+  id INTEGER PRIMARY KEY,
+  budget_id INTEGER NOT NULL REFERENCES budget (id) ON DELETE CASCADE,
+  name TEXT NOT NULL CHECK (LENGTH(name) >= 3),
+  category_group_id INTEGER,
+  CHECK (lower(name) IS NOT 'income'),
+  UNIQUE (budget_id, id),
+  UNIQUE (budget_id, name),
+  FOREIGN KEY (budget_id, category_group_id) REFERENCES category_group (budget_id, id) ON DELETE CASCADE
 );
 
 CREATE TABLE category_group (
-    id INTEGER PRIMARY KEY,
-    budget INTEGER NOT NULL REFERENCES budget (id) ON DELETE CASCADE,
-    name TEXT NOT NULL CHECK (name <> ''),
-    UNIQUE (budget, name)
+  id INTEGER PRIMARY KEY,
+  budget_id INTEGER NOT NULL REFERENCES budget (id) ON DELETE CASCADE,
+  name TEXT NOT NULL CHECK (LENGTH(name) >= 3),
+  UNIQUE (budget_id, id),
+  UNIQUE (budget_id, name)
 );
 
-CREATE TABLE "payee_default_category" (
-    id INTEGER PRIMARY KEY,
-    payee INTEGER NOT NULL REFERENCES payee (id) ON DELETE CASCADE,
-    other_account INTEGER REFERENCES account (id) ON DELETE CASCADE,
-    category INTEGER REFERENCES category (id) ON DELETE CASCADE,
-    income BOOL NOT NULL DEFAULT false,
-    percent INTEGER NOT NULL CHECK (percent >= 0 AND percent <= 100),
-    CHECK (
-        (CASE WHEN other_account IS NOT NULL THEN 1 ELSE 0 END
-       + CASE WHEN category IS NOT NULL THEN 1 ELSE 0 END
-       + CASE WHEN income THEN 1 ELSE 0 END) = 1
-    )
+CREATE TABLE payee_default_line (
+  id INTEGER PRIMARY KEY,
+  budget_id INTEGER NOT NULL,
+  payee_id INTEGER NOT NULL,
+  dest_account_id INTEGER,
+  category_id INTEGER,
+  income BOOL NOT NULL DEFAULT false,
+  percent INTEGER NOT NULL,
+  -- XOR, can only be one of category (spend), transfer, or income
+  CHECK (
+    (
+      CASE
+        WHEN dest_account_id IS NOT NULL THEN 1
+        ELSE 0
+      END + CASE
+        WHEN category_id IS NOT NULL THEN 1
+        ELSE 0
+      END + CASE
+        WHEN income THEN 1
+        ELSE 0
+      END
+    ) = 1
+  ),
+  CHECK (
+    percent > 0
+    AND percent <= 100
+  ),
+  FOREIGN KEY (budget_id, payee_id) REFERENCES payee (budget_id, id) ON DELETE CASCADE,
+  FOREIGN KEY (budget_id, dest_account_id) REFERENCES account (budget_id, id) ON DELETE CASCADE,
+  FOREIGN KEY (budget_id, category_id) REFERENCES category (budget_id, id) ON DELETE CASCADE
 );
 
 CREATE TABLE allocation (
-    id INTEGER PRIMARY KEY,
-    month UNIX_EPOCH_INTEGER NOT NULL,
-    budget INTEGER NOT NULL REFERENCES budget (id) ON DELETE CASCADE,
-    category INTEGER NOT NULL REFERENCES category (id) ON DELETE CASCADE,
-    amount INTEGER NOT NULL, 
-    UNIQUE (budget, category, month)
+  id INTEGER PRIMARY KEY,
+  budget_id INTEGER NOT NULL,
+  category_id INTEGER NOT NULL,
+  month UNIX_EPOCH_INTEGER NOT NULL,
+  amount INTEGER NOT NULL,
+  UNIQUE (budget_id, category_id, month),
+  FOREIGN KEY (budget_id, category_id) REFERENCES category (budget_id, id) ON DELETE CASCADE
 );
 
 CREATE TABLE reconciliation (
-    account        INTEGER NOT NULL REFERENCES account (id) ON DELETE CASCADE,
-    "transaction"  INTEGER NOT NULL REFERENCES "transaction" (id) ON DELETE CASCADE,
-    PRIMARY KEY (account, "transaction")
+  id INTEGER PRIMARY KEY,
+  budget_id INTEGER NOT NULL,
+  account_id INTEGER NOT NULL,
+  trx_id INTEGER REFERENCES trx (id) ON DELETE CASCADE,
+  trx_line_id INTEGER REFERENCES trx_line (id) ON DELETE CASCADE,
+  CHECK (
+    (
+      CASE
+        WHEN trx_id IS NOT NULL THEN 1
+        ELSE 0
+      END + CASE
+        WHEN trx_line_id IS NOT NULL THEN 1
+        ELSE 0
+      END
+    ) = 1
+  ),
+  FOREIGN KEY (budget_id, account_id) REFERENCES account (budget_id, id) ON DELETE CASCADE
 );
 
-CREATE TABLE "goal" (
-    id       INTEGER PRIMARY KEY,
-    budget   INTEGER NOT NULL REFERENCES budget (id) ON DELETE CASCADE,
-    type     TEXT  NOT NULL CHECK (type IN ('monthly', 'save', 'refill')),
-    start    UNIX_EPOCH_INTEGER    NOT NULL,
-    "end"    UNIX_EPOCH_INTEGER,
-    category INTEGER NOT NULL REFERENCES category (id) ON DELETE CASCADE,
-    amount   INTEGER NOT NULL,
-    UNIQUE (budget, category),
-    CHECK (type IN ('monthly', 'refill') OR "end" IS NOT NULL),
-    CHECK ("end" IS NULL OR "end" >= start),
-    CHECK (amount > 0)
+CREATE TABLE goal (
+  id INTEGER PRIMARY KEY,
+  budget_id INTEGER NOT NULL,
+  category_id INTEGER NOT NULL,
+  type TEXT NOT NULL CHECK (type IN ('monthly', 'save', 'refill')),
+  start_date UNIX_EPOCH_INTEGER NOT NULL,
+  end_date UNIX_EPOCH_INTEGER,
+  amount INTEGER NOT NULL,
+  UNIQUE (budget_id, category_id),
+  CHECK (
+    type IN ('monthly', 'refill')
+    OR end_date IS NOT NULL
+  ),
+  CHECK (
+    end_date IS NULL
+    OR end_date >= start_date
+  ),
+  CHECK (amount > 0),
+  FOREIGN KEY (budget_id, category_id) REFERENCES category (budget_id, id) ON DELETE CASCADE
 );
+
+-- +goose down
+DROP TABLE goal;
+
+DROP TABLE reconciliation;
+
+DROP TABLE allocation;
+
+DROP TABLE payee_default_line;
+
+DROP TABLE category_group;
+
+DROP TABLE category;
+
+DROP TABLE trx_line;
+
+DROP TABLE trx;
+
+DROP TABLE payee;
+
+DROP TABLE account;
+
+DROP TABLE budget;
+
+DROP TABLE login;
--- migrations/00002_account_view.sql
+++ migrations/00002_account_view.sql
@@ -1,47 +1,84 @@
 -- +goose up
-
-CREATE VIEW account_transactions AS
+CREATE VIEW account_trx AS
 SELECT
+  a.budget_id as budget_id,
   a.id as account_id,
-  t.id  as transaction_id,
-  CAST(NULL AS INTEGER) as source_account,
+  t.id as trx_id,
+  CAST(NULL AS INTEGER) as source_account_id,
   t.date as date,
-  t.payee as payee,
+  t.payee_id as payee_id,
   t.total_inflow as inflow,
   t.total_outflow as outflow,
   COALESCE(t.note, '') as note,
-  EXISTS(SELECT true FROM reconciliation as r WHERE r.account = a.id AND t.id = r."transaction" LIMIT 1) as reconciled
-FROM account AS a
-JOIN "transaction" AS t ON t.account = a.id
+  EXISTS (
+    SELECT
+      true
+    FROM
+      reconciliation as r
+    WHERE
+      r.account_id = a.id
+      AND t.id = r.trx_id
+    LIMIT
+      1
+  ) as reconciled
+FROM
+  account AS a
+  JOIN trx AS t ON t.account_id = a.id
 UNION ALL
 SELECT
+  a.budget_id as budget_id,
   a.id as account_id,
-  tcp.id  as transaction_id,
-  tcp.account as source_account,
+  tcp.id as trx_id,
+  tcp.account_id as source_account_id,
   tcp.date as date,
-  tcp.payee as payee,
+  tcp.payee_id as payee_id,
   tc.outflow as inflow,
   tc.inflow as outflow,
   COALESCE(tcp.note, '') as note,
-  EXISTS(SELECT true FROM reconciliation as r WHERE r.account = a.id AND tcp.id = r."transaction" LIMIT 1) as reconciled
-FROM account AS a
-JOIN "transaction_category" AS tc ON tc.other_account = a.id
-JOIN "transaction" AS tcp ON tcp.id = tc."transaction";
+  EXISTS (
+    SELECT
+      true
+    FROM
+      reconciliation as r
+    WHERE
+      r.account_id = a.id
+      AND tc.id = r.trx_line_id
+    LIMIT
+      1
+  ) as reconciled
+FROM
+  account AS a
+  JOIN trx_line AS tc ON tc.dest_account_id = a.id
+  JOIN trx AS tcp ON tcp.id = tc.trx_id;
 
 CREATE VIEW account_balances AS
 SELECT
-   a.id,
-   a.name,
-   CAST(COALESCE(SUM(at.inflow - at.outflow), 0) AS INTEGER) AS balance,
-   CAST(COALESCE(SUM(CASE 
-        WHEN at.reconciled THEN at.inflow - at.outflow
-        ELSE 0
-   END), 0) AS INTEGER) AS reconciled_balance
-FROM account AS a
-LEFT JOIN account_transactions as at ON a.id = at.account_id
-GROUP BY a.id, a.name;
+  a.budget_id,
+  a.id AS account_id,
+  a.name,
+  CAST(
+    COALESCE(SUM(at.inflow - at.outflow), 0) AS INTEGER
+  ) AS balance,
+  CAST(
+    COALESCE(
+      SUM(
+        CASE
+          WHEN at.reconciled THEN at.inflow - at.outflow
+          ELSE 0
+        END
+      ),
+      0
+    ) AS INTEGER
+  ) AS reconciled_balance
+FROM
+  account AS a
+  LEFT JOIN account_trx as at ON a.id = at.account_id
+GROUP BY
+  a.budget_id,
+  a.id,
+  a.name;
 
 -- +goose down
+DROP VIEW account_trx;
 
-DROP VIEW account_transactions;
 DROP VIEW account_balances;
